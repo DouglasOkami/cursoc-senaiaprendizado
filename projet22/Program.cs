c62b7efc8d2aac51f7152344d5e211403ae9458b
DEV <dev@info.local>
2021-09-02 17:08:28 -0300

    projeto 23 finalizado

--- projet22/Program.cs
+++ projet22/Program.cs
@@ -7,7 +7,7 @@ namespace projeto22
         static void Main(string[] args)
         {
 
-            double maior = 0, menor = 0;
+            /*double maior = 0, menor = 0;
             double[] temp = new double[12];
 
             for (int i = 0; i < 12; i++)
@@ -30,6 +30,36 @@ namespace projeto22
             }// fim for
             Console.WriteLine($"A menor temperatura de todos os meses foi: {menor}");
             Console.WriteLine($"A maior temperatura de todos os meses foi: {maior}");
-        }
+            */
+
+
+            double[] mediasTemp = new double[12];
+            string[] mes = {"Jan","Fev","Marc","Abril","Maio","Junho","Jul","Agos","Setem","Outubr","Novem","Dezem"};
+            for (var i = 0; i < 12; i++)
+            {
+                Console.WriteLine($"Digite a temperatur a do {i + 1} mês");
+                mediasTemp[i] = double.Parse(Console.ReadLine());
+            }
+            double maiorTemp = mediasTemp[0];
+            double menorTemp = mediasTemp[0];
+            int contador = 0 , contadorMaior = 0 , contadorMenor = 0;
+            foreach (var item in mediasTemp)
+            {
+                if (maiorTemp < item)
+                {
+                    maiorTemp = item;
+                    contadorMaior = contador;
+                }
+                if (menorTemp > item)
+                {
+                    menorTemp = item;
+                    contadorMenor = contador;
+                }
+                contador++;
+            }// fim foreach
+
+            Console.WriteLine($"A maior temperatura = {maiorTemp} e ocorreu no mês {mes[contadorMaior]}");
+            Console.WriteLine($"A menor temperatura = {menorTemp} e ocorreu no mês {mes[contadorMenor]}");
+        }// fim main
     }
 }
