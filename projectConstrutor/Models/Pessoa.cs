727658e782b8a02ada2839d96cb9896d3d0ce0af
Douglas Okami <meliodasbrpjt1@gmail.com>
2021-09-15 13:21:04 -0300

    novo projeto criado , nome projectConstrutor

---
+++ projectConstrutor/Models/Pessoa.cs
@@ -0,0 +1,10 @@
+namespace projectConstrutor.Models
+{
+    public class Pessoa
+    {
+        public string nome;
+        public string sobrenome;
+        public string telefone;
+        public string idade;
+    }
+}
---
+++ projectConstrutor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using projectConstrutor.Models;
+
+namespace projectConstrutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            
+        }
+    }
+}
