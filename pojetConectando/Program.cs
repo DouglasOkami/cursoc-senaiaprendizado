1b0742c2e8773757a3956767b305193159dfc0a9
Douglas Okami <meliodasbrpjt1@gmail.com>
2021-09-28 15:54:00 -0300

    arquivos do novo projeto

---
+++ pojetConectando/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConectandoComBancoD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestarConexao();
+        }//fim main
+        static void TestarConexao()
+        {
+            try
+            {
+                //Tenta executar oque tem aqui dentro 
+                var conn = Conexao.GetConnection();//aqui me retorna o link da conexão
+                conn.Open();
+                Console.WriteLine("Abriu a porta da esperança");
+                conn.Close();
+                Console.WriteLine("Fechou a porta da esperança");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Mensagem de erro"+ex.Message);
+            }
+        }//Fim Testar
+    }
+}
