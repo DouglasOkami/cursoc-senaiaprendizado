6d5cc33c5d6f0778e2d2247b6655e4e5741aa548
Douglas Okami <meliodasbrpjt1@gmail.com>
2021-10-06 14:40:17 -0300

    implementando novos código no projectMVC

--- projectMVCRazerCRUD/MVCRazerCRUD/Models/Aluno.cs
+++ projectMVCRazerCRUD/MVCRazerCRUD/Models/Aluno.cs
@@ -12,12 +12,10 @@ namespace MVCRazerCRUD.Models
     public class Aluno : UsuarioBase , IAluno
     {
         public string Escolaridade { get; set; }
-
         public Aluno AlterarAluno(Aluno aluno)
         {
             throw new NotImplementedException();
         }
-
         public Aluno CasdastrarAluno(Aluno aluno)
         {
             try
@@ -45,12 +43,38 @@ namespace MVCRazerCRUD.Models
             {
                 throw;
             }
-        }
-
+        }//Fim Cadastrar
         public List<Aluno> ListarAluno()
         {
-            throw new NotImplementedException();
-        }
+            //Definir a conexão - Precisamos criar a classe de conexão
+            var connection = Conexao.GetSqlConnection();
+            connection.Open();
+            //query
+            var query = "select * from alunos";
+            var command = new SqlCommand(query, connection);
+            var dataSet = new DataSet();
+            var adapter = new SqlDataAdapter(command);
+            adapter.Fill(dataSet);
+
+            var rows = dataSet.Tables[0].Rows;
+
+            List<Aluno> listaDeAlunos = new List<Aluno>();
+            foreach (DataRow item in rows)
+            {
+                Aluno aluno = new Aluno();
+                var colunas = item.ItemArray;
+                aluno.Id = int.Parse(colunas[0].ToString());
+                aluno.Nome = colunas[1].ToString();
+                aluno.Email = colunas[2].ToString();
+                aluno.Endereco = colunas[3].ToString();
+                aluno.Telefone = colunas[4].ToString();
+                aluno.Escolaridade = colunas[5].ToString();
+
+                listaDeAlunos.Add(aluno);
+            }//fim foreach
+            connection.Close();
+            return listaDeAlunos;
+        }// fim ListarAluno
 
         public Aluno RemoverAluno(int id)
         {
